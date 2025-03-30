4bd4b2b51a4859a9a662d27c58037be2a00ae522
Zoe <zrobinso@andrew.cmu.edu>
2025-03-30 12:10:55 -0400

    validating form responses; still fixing DOM elemnt

--- static/script.js
+++ static/script.js
@@ -26,9 +26,22 @@ if (locationPage.includes("index.html")) {
 // INFOMRATION TO KNOW TODAY
 function updateElement(){
     var info = document.getElementById("infoForToday");
-    var arrayOfInfo = ["The weather can change, make sure to bring a coat!", "Check out our gift shop for fun souvenirs!", "We offer free tours for high schools!", "We offer a fresh view of Pittsburgh from our cable car!"];
+    var arrayOfInfo = ["The weather can change, make sure to bring a coat!", "Check out our gift shop for fun souvenirs!", "We offer tours for high schools!", "We offer a fresh view of Pittsburgh from our cable car!"];
     var randomInfo = arrayOfInfo[Math.floor(Math.random() * arrayOfInfo.length)];
     info.innerHTML = randomInfo;
 }
 setInterval(updateElement, 3600000); //found documentaiton for this
 updateElement();
+
+//form validation
+function validate(){
+    var name = document.getElementById("name");
+    var lastName = document.getElementById("last");
+    var email = document.getElementById("email");
+    var phone = document.getElementById("phone");
+    var zip = document.getElementById("zip");
+    var msg = document.getElementById("ValidateMsg");
+    if(!name.checkValidity() || !lastName.checkValidity() || !email.checkValidity() || !phone.checkValidity() || !zip.checkValidity()){
+        msg.innerHTML = "Please fill out the form correctly so I can get back to you.";
+    }
+}
