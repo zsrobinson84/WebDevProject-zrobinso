b493b5d311d53bbe56c2cfdcbd7c038c167a64cd
Zoe <zrobinso@andrew.cmu.edu>
2025-03-30 19:51:59 -0400

    added API

--- static/script.js
+++ static/script.js
@@ -24,14 +24,15 @@ if (locationPage.includes("index.html")) {
 }
 
 // INFOMRATION TO KNOW TODAY
-function updateElement(){
-    var info = document.getElementById("infoForToday");
-    var arrayOfInfo = ["The weather can change, make sure to bring a coat!", "Check out our gift shop for fun souvenirs!", "We offer tours for high schools!", "We offer a fresh view of Pittsburgh from our cable car!"];
-    var randomInfo = arrayOfInfo[Math.floor(Math.random() * arrayOfInfo.length)];
-    info.innerHTML = randomInfo;
-}
-setInterval(updateElement, 3600000); //found documentaiton for this
-updateElement();
+// function updateElement(){
+//     var info = document.getElementById("infoForToday");
+//     var arrayOfInfo = ["The weather can change, make sure to bring a coat!", "Check out our gift shop for fun souvenirs!", "We offer tours for high schools!", "We offer a fresh view of Pittsburgh from our cable car!"];
+//     var randomInfo = arrayOfInfo[Math.floor(Math.random() * arrayOfInfo.length)];
+//     info.innerHTML = randomInfo;
+// }
+setInterval(getTemperature, 3600000); //found documentaiton for this
+getTemperature();
+// updateElement();
 
 
 //JQUERY FOR INTERACTIVE PIECE (HOME PAGE)
@@ -44,3 +45,29 @@ $(document).ready(function(){
 });
 
 
+// API
+function getTemperature() {
+    const apiKey = "2b768f8f17b7869549fe38dba51c48c9";
+    const city = "Pittsburgh";
+    const url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${city}`;
+    var info = document.getElementById("infoForToday");
+
+    fetch(url)
+        .then(response => response.json()) // Convert response to JSON
+        .then(data => {
+            if (data.success === false) {
+                throw new Error(data.error.info); // Handle API errors
+            }
+            const temperature = data.current.temperature; // Extract temperature
+            temperatureF = convertToFahrenheit(temperature);
+            info.innerText = `Current temperature in Pittsburgh: ${temperatureF}°F`;
+        })
+        .catch(error => {
+            console.error("Error fetching weather data:", error);
+            info.innerText = "Oops! Something went wrong. Try again.";
+        });
+}
+
+function convertToFahrenheit(celsius) {
+    return celsius * 9 / 5 + 32;
+}
